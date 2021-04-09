2dcf9c502ead2bea4c1439af4c04719b90479da5
Oscar <oorellana30.95@gmail.com>
2021-04-09 18:46:31 +0200

    Lint

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -12,7 +12,7 @@ test('renders with App and root div', () => {
   document.body.appendChild(root);
 
   // Requires index.js so that react-dom render method is called
-  require('./index.js');
+  require('./index.tsx');
 
   // Asserts render was called with <App />
   // and HTML element with id = root
