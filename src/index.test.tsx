39b2d40d6a040588ad13297c846b8c6ad45fbf43
Oscar <oorellana30.95@gmail.com>
2021-04-09 18:41:44 +0200

    Add coverage

---
+++ src/index.test.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+jest.mock('react-dom', () => ({ render: jest.fn() }));
+
+test('renders with App and root div', () => {
+  // Create and append to document body
+  // an HTML element with id = root
+  const root = document.createElement('div');
+  root.id = 'root';
+  document.body.appendChild(root);
+
+  // Requires index.js so that react-dom render method is called
+  require('./index.js');
+
+  // Asserts render was called with <App />
+  // and HTML element with id = root
+  expect(ReactDOM.render).toHaveBeenCalledWith(<App />, root);
+});
